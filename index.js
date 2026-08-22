fade8feefd47d7eafcfc7a44bdc45b80b6b73ee3
Shivani Meena <meenashivani133@gmail.com>
2026-08-22 22:46:27 +0530

    Add Dockerized Express app

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from my Express Docker app!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
